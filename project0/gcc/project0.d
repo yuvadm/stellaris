44b979ac3dc53b29ee8176517b7d9a10935630c0
Yuval Adam <yuv.adm@gmail.com>
2012-10-29 21:36:23 +0200

    Initial code

---
+++ project0/gcc/project0.d
@@ -0,0 +1,2 @@
+gcc/project0.o: project0.c ../inc/hw_types.h ../inc/hw_memmap.h \
+ ../driverlib/sysctl.h ../driverlib/gpio.h ../driverlib/pin_map.h
---
+++ project0/gcc/startup_gcc.d
@@ -0,0 +1,1 @@
+gcc/startup_gcc.o: startup_gcc.c ../inc/hw_nvic.h ../inc/hw_types.h
